7ea2c3de7b42dd1781e6548ca3790d112ff59002
Ian Smith <imsmith1995@gmail.com>
2023-07-11 14:30:43 -0400

    Add project files.

---
+++ L06_02/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace L06_02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blackToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.redToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.blackToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.blueToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.redToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.lblOut = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.colorToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 42);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // colorToolStripMenuItem
+            // 
+            this.colorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.blackToolStripMenuItem,
+            this.blueToolStripMenuItem,
+            this.redToolStripMenuItem});
+            this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            this.colorToolStripMenuItem.Size = new System.Drawing.Size(91, 38);
+            this.colorToolStripMenuItem.Text = "Color";
+            // 
+            // blackToolStripMenuItem
+            // 
+            this.blackToolStripMenuItem.Name = "blackToolStripMenuItem";
+            this.blackToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.blackToolStripMenuItem.Text = "Black";
+            this.blackToolStripMenuItem.Click += new System.EventHandler(this.blackToolStripButton_Click);
+            // 
+            // blueToolStripMenuItem
+            // 
+            this.blueToolStripMenuItem.Name = "blueToolStripMenuItem";
+            this.blueToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.blueToolStripMenuItem.Text = "Blue";
+            this.blueToolStripMenuItem.Click += new System.EventHandler(this.blueToolStripButton_Click);
+            // 
+            // redToolStripMenuItem
+            // 
+            this.redToolStripMenuItem.Name = "redToolStripMenuItem";
+            this.redToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.redToolStripMenuItem.Text = "Red";
+            this.redToolStripMenuItem.Click += new System.EventHandler(this.redToolStripButton_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.blackToolStripButton,
+            this.blueToolStripButton,
+            this.redToolStripButton});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 42);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 42);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(213, 36);
+            this.toolStripLabel1.Text = "Choose Text Color:";
+            // 
+            // blackToolStripButton
+            // 
+            this.blackToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.blackToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.blackToolStripButton.Name = "blackToolStripButton";
+            this.blackToolStripButton.Size = new System.Drawing.Size(73, 36);
+            this.blackToolStripButton.Text = "Black";
+            this.blackToolStripButton.Click += new System.EventHandler(this.blackToolStripButton_Click);
+            // 
+            // blueToolStripButton
+            // 
+            this.blueToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.blueToolStripButton.ForeColor = System.Drawing.Color.Blue;
+            this.blueToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.blueToolStripButton.Name = "blueToolStripButton";
+            this.blueToolStripButton.Size = new System.Drawing.Size(65, 36);
+            this.blueToolStripButton.Text = "Blue";
+            this.blueToolStripButton.Click += new System.EventHandler(this.blueToolStripButton_Click);
+            // 
+            // redToolStripButton
+            // 
+            this.redToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.redToolStripButton.ForeColor = System.Drawing.Color.Crimson;
+            this.redToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.redToolStripButton.Name = "redToolStripButton";
+            this.redToolStripButton.Size = new System.Drawing.Size(58, 36);
+            this.redToolStripButton.Text = "Red";
+            this.redToolStripButton.Click += new System.EventHandler(this.redToolStripButton_Click);
+            // 
+            // lblOut
+            // 
+            this.lblOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOut.Location = new System.Drawing.Point(0, 98);
+            this.lblOut.Name = "lblOut";
+            this.lblOut.Size = new System.Drawing.Size(800, 355);
+            this.lblOut.TabIndex = 2;
+            this.lblOut.Text = "A simple program using ToolStrip and MenuStrip";
+            this.lblOut.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblOut);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Lesson 06 Solution 02";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blackToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blueToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem redToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripButton blackToolStripButton;
+        private System.Windows.Forms.ToolStripButton blueToolStripButton;
+        private System.Windows.Forms.ToolStripButton redToolStripButton;
+        private System.Windows.Forms.Label lblOut;
+    }
+}
+
---
+++ L06_02/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace L06_02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void blackToolStripButton_Click(object sender, EventArgs e)
+        {
+            lblOut.ForeColor = Color.Black;
+        }
+
+        private void blueToolStripButton_Click(object sender, EventArgs e)
+        {
+            lblOut.ForeColor = Color.Blue;
+        }
+
+        private void redToolStripButton_Click(object sender, EventArgs e)
+        {
+            lblOut.ForeColor = Color.Crimson;
+        }
+    }
+}
